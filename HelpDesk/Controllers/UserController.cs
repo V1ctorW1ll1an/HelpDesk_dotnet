80a5a4890597c015a432ed67b7ace5e8e5800f80
Victor Willian <victorwillian1012@gmail.com>
2023-05-03 00:37:19 -0300

    chore: :white_check_mark: dapper

--- HelpDesk/Controllers/UserController.cs
+++ HelpDesk/Controllers/UserController.cs
@@ -1,17 +1,28 @@
+using Dapper;
+using HelpDesk.Models;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
 namespace HelpDesk.Controllers;
-
-// form action=/user
-
 public class UserController : Controller
 {
-
-  public UserController() { }
+  private readonly IConfiguration _configuration;
+  public UserController(IConfiguration configuration)
+  {
+    _configuration = configuration;
+  }
 
   // GET default controller /user/
-  public IActionResult Index(string id)
+  public async Task<ActionResult<List<UserViewModel>>> Index()
   {
-    return View();
+    var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+    using var connection = new NpgsqlConnection(connectionString);
+    connection.Open();
+
+    var users = await connection.QueryAsync<UserViewModel>("SELECT * FROM tb_usuario");
+
+    return View(users.ToList());
+
   }
 }
---
+++ HelpDesk/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+namespace HelpDesk.Models
+{
+  public class UserViewModel
+  {
+    public string? id_usuario { get; set; }
+    public string? nome_usuario { get; set; }
+    public string? email_usuario { get; set; }
+    public string? senha_usuario { get; set; }
+    public string? id_perfil { get; set; }
+  }
+}
